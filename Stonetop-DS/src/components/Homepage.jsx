b823b90c5e115aec2fc6b912d8461da41dc1af73
Nick <Echophase44@github.com>
2023-04-06 18:59:41 -0400

    Added hero spalsh image and some text/styling.

--- Stonetop-DS/src/components/Homepage.jsx
+++ Stonetop-DS/src/components/Homepage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import '../styles/homepage.css'
+import Hero from '../assets/mainOffice.jpg'
+import Logo from "../assets/logo.png"
+
 
 function Homepage() {
   return (
-    <div className="test">
-      test
-    </div>
+    <>
+      <section className="home_hero-container">
+        <img className="home_hero" src={Hero} alt="Office" />
+        <img className="home_logo" src={Logo} alt="Company Logo" />
+        <div className="home_title">Stonetop Financial Solutions</div>
+      </section>
+      <section className="home_about-container">
+
+      </section>
+
+    </>
   )
 }
 
